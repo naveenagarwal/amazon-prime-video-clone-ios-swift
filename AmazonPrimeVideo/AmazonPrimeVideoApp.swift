c6517741203d01a65573ba28c34cb491a0b66205
Naveen Agarwal <naveen.agarwal@srijan.net>
2021-07-31 12:47:33 +0530

    Initial Commit

---
+++ AmazonPrimeVideo/AmazonPrimeVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmazonPrimeVideoApp.swift
+//  AmazonPrimeVideo
+//
+//  Created by Naveen on 31/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AmazonPrimeVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
